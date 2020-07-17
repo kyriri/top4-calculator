ed9e7f7a8c4fa249d444480995627216b49d7a75
Kyriri <736dccb6@opayq.com>
2020-07-16 23:45:00 -0300

    Eliminate the 'usher' concept
    Eventually, it was not necessary. This will make the code logic simpler

--- first.js
+++ first.js
@@ -2,7 +2,6 @@
 // Author page: https://github.com/kyriri
 
 const display = document.getElementsByClassName('display-inner')[0];
-let usher = 0;
 let memory = [
   null, // number position
   null, // function position
@@ -64,56 +63,42 @@ function placeInput(input) {
   if (input.value == 'restart') { restart(); return; }
   if (input.value == 'clear') { clear(); return; }
 
-  if (usher % 2 === 0) {       // usher points to number position...
-    if (input.type == 'num') { 
-      if (memory[usher] && memory[usher].length > 10) return;
-      if (memory[usher] && input.value === '.' && memory[usher].match(/\./)) return;
-      if (memory[usher] === null) memory[usher] = input.value;
-      else memory[usher] += input.value;
-    } 
-    if (input.type == 'fn') { 
-      if (usher === 0) {
-        if (memory[0] === null && input.value != 'minus') return;
-        if (memory[0] === null && input.value == 'minus') {
-          memory[0] = '-';
-          usher = 0;
-        } else {
-          memory[1] = input.value;
-          usher = 1;
-        }
-      } 
-      if (usher === 2) {
-        if (memory[2] === null) memory[1] = input.value;
-        else {
-          memory[0] = calculate(memory[0], memory[1], memory[2]);
-          if (memory[0] == 'Error') {
-            restart();
-            updateDisplay('Error');
-          } else {
-            memory[1] = input.value;
-            memory[2] = null;
-            usher = 1;
-          }
-        }
-      }
+  if (input.type == 'num') {
+    if (memory[1] == 'equals') {
+      memory[0] = null;
+      memory[1] = null;
     }
-  } else { // usher points to function position (that is, 1)
-    if (input.type == 'num') {
-      if (memory[1] == 'equals') {
-        memory[0] = input.value;
-        restart('equals');
+    let position = memory[1] === null ? 0 : 2;
+    if (input.value == '.' && memory[position] === null) memory[position] = '0';
+    
+    if (memory[position] === null) memory[position] = input.value;
+    else {
+      if (input.value == '.' && memory[position].includes('.')) return;
+      memory[position] += input.value;
+    }
+  }
+  if (input.type == 'fn') {
+    if (!memory[0]) {
+      if (input.value == 'minus') memory[0] = '-';
+      else return;
+    }
+    else if (!memory[2]) memory[1] = input.value;
+    else {
+      memory[0] = calculate(memory[0], memory[1], memory[2]);
+      if (memory[0] == 'Error') {
+        restart();
+        updateDisplay('Error');
+        return;
       } else {
-        memory[2] = input.value;
-        usher = 2;
+        memory[1] = input.value;
+        memory[2] = null;
       }
-    } else {
-      memory[1] = input.value;
     }
   }
   updateDisplay();
 }
 function updateDisplay(info) {
-  display.innerHTML = info ? info : memory.reduce( (acc, item) => { 
+  display.innerHTML = info || memory.reduce( (acc, item) => { 
     let char;
     if (item === null) char = '';
     else {
@@ -134,38 +119,35 @@ function updateDisplay(info) {
           char = '';
           break;   
         default:
-          char = item;
+          char = item.toLocaleString();
       }
     } 
     return acc + char;
-  }, '');
+  }, '') || '0';
 }
-function restart(exception) {
-  if (!exception) memory[0] = null; 
+function restart(equals) {
+  if (!equals) memory[0] = null; 
   memory[1] = null;
   memory[2] = null;
-  usher = 0;
   updateDisplay('0');
 }
 function clear() {
+  // erases the last input on the last non-empty memory position
   switch (true) {
     case (memory[2] !== null):
       if (memory[2].length === 1) {
         memory[2] = null;
-        usher = 1;
       } else memory[2] = memory[2].slice(0,-1);
       break;
     case (memory[1] !== 'equals' && memory[1] !== null):
       memory[1] = null;
-      usher = 0;
       break;
     case (memory[1] == 'equals'):
       memory[1] = null;
     default:
-      if (memory[0] === null) return;
+      if (memory[0] === null) return; 
       else if (memory[0].length === 1) memory[0] = null;
       else memory[0] = memory[0].slice(0,-1);
-      usher = 0;
   }
   updateDisplay();
 }
@@ -187,10 +169,14 @@ function calculate(num1 = 0, fn, num2 = 0) {
       result = num1 / num2;
     }
   if (isNaN(result) || Math.abs(result) === Infinity) return 'Error';
-  if (String(result).match(/.*\..*0{7,}/)) result = String(result).slice(0, String(result).match(/0{7,}/).index);
-  // above, to avoid float point issues (such as 0.1 + 0.2 != 0.3)
+  
+  // below, to avoid float point issues (such as 0.1 + 0.2 != 0.3)
   // if after a dot there are 7 or more zeros in a row, 
   // it ignores everything after the first zero
+  if (String(result).match(/.*\..*0{7,}/)) result = String(result).slice(0, String(result).match(/0{7,}/).index);
+  
+  // big numbers get converted to scientific notation,
+  // and numbers with many decimals get rounded to the last decimal place the display can show
   if (Math.abs(result) >= 1e11) result = result.toExponential(5);
   else if (
     String(result).length > 11 &&
